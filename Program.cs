4b27a02469988e430945c5cdb85f3fb641d9e238
lena <sola-2011-s@hotmail.com>
2025-09-22 10:36:40 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace CodeFirstStudentApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                // إنشاء طالب جديد
+                var student = new Student { StudentName = "Lena" };
+
+                // إضافة الطالب لقاعدة البيانات
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Student has been added successfully!");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    // الكلاس Student
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    // الكلاس DbContext
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace CodeFirstStudentApp
+{
+    // This is your model
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    // This is your DbContext
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
